ac4213d403c8163b39e46cc5a29642e8d63cf56e
hiraygui <hiraygui@gmail.com>
2022-04-25 18:32:35 -0300

    alterando cores e adicionando seção de produto

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+    //função da barra de navegação lateral
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
 });
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+    $('.slider').slider();
+  });
